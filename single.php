aea896092c8fa6d69bf61bd57e04b146f5a93256
Bruce Elgort <belgort@clark.edu>
2023-05-06 15:59:49 -0700

    Spring 2023

--- single.php
+++ single.php
@@ -1,32 +1,35 @@
-<?php
-$pageTitle = "Blog Home";
-require "inc/header.inc.php";
+<?php require "inc/header.inc.php";
+
+if (!isset($_GET['id'])) {
+    header("Location: blog.php");
+} else {
+    $blog_id = $_GET['id'];
+}
 ?>
-
-<div class="container mt-3">
+<div class="container">
     <div class="row">
-        <h1 class="mb-5">Bruce's Technology Blog</h1>
-        <?php
-        // SQL to get all blog posts. Note the use of a JOIN
-        $sql = "SELECT post.post_id, post.title, post.date, post.content, author.author_id, author.first_name, author.last_name 
-                    FROM post 
-                    JOIN author 
-                    ON post.author = author.author_id";
+        <div class="col-12">
+            <?php
+            // SQL to get a single blog post. Note the use of a JOIN
+            $sql = "SELECT post.post_id, post.title, post.date, post.content, author.author_id, author.first_name, author.last_name 
+                FROM post 
+                JOIN author 
+                ON post.author = author.author_id 
+                WHERE post.post_id = :blog_id";
 
-        // PDO Prepared Statements
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
+            // PDO Prepared Statements
+            $stmt = $db->prepare($sql);
+            $stmt->execute(["blog_id" => $blog_id]);
 
-        // Fetch all of the row(s)
-        $data = $stmt->fetchAll();
+            // Fetch one row
+            $row = $stmt->fetch();
 
-        // Iterate through each of the rows
-        foreach ($data as $row) {
-            // Create HTML for each blog entry
-            echo "<div class='col-12 mb-5'>";
+            // var_dump($data);
+
             // Blog Title
             echo "<h2>{$row->title}</h2>";
             echo "<hr>";
+
             // Take the date and convert it to a PHP date object
             $date = date_create($row->date);
             // Show blog post author and format the date
@@ -34,10 +37,10 @@ require "inc/header.inc.php";
 
             // Now get the categories for this post with SQL JOIN
             $sql = "SELECT post_category.post_id, post_category.category_id, category.category 
-                    FROM post_category 
-                    JOIN category 
-                    ON post_category.category_id = category.category_id 
-                    WHERE post_category.post_id = :post_id";
+                FROM post_category 
+                JOIN category 
+                ON post_category.category_id = category.category_id 
+                WHERE post_category.post_id = :post_id";
 
             // PDO Prepared statements
             $stmt_category = $db->prepare($sql);
@@ -54,10 +57,10 @@ require "inc/header.inc.php";
 
             // Now get the tags for this post with SQL JOIN
             $sql = "SELECT post_tag.post_id, post_tag.tag_id, tag.tag 
-                    FROM post_tag 
-                    JOIN tag 
-                    ON post_tag.tag_id = tag.id 
-                    WHERE post_tag.post_id = :post_id";
+                FROM post_tag 
+                JOIN tag 
+                ON post_tag.tag_id = tag.id 
+                WHERE post_tag.post_id = :post_id";
 
             // PDO Prepared statements
             $stmt_tag = $db->prepare($sql);
@@ -66,6 +69,7 @@ require "inc/header.inc.php";
 
             // Generate an unordered list with tags
             echo "<p>Tag(s)</p>";
+
             echo "<ul>";
             foreach ($tags as $tag_row) {
                 echo "<li>{$tag_row->tag}</li>";
@@ -73,13 +77,10 @@ require "inc/header.inc.php";
             echo "</ul>";
 
             // Show the blog post content
-            $content = limit_text($row->content, 15);
-            echo "<p>{$content}</p>";
-            echo "<a href='single.php?id={$row->post_id}' title='Read the post'>Read more ></a>";
-            echo "</div>"; // closing .col-12
-        } // end of loop for Posts
-        ?>
-    </div> <!-- Closing for .row -->
+            echo "<p>{$row->content}</p>";
+            ?>
+        </div>
+    </div>
     <?php
     require "inc/footer.inc.php";
     ?>
